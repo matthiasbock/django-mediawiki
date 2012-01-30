9122d401706eafc62dd47ad4d2546928452702f3
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@7db8904a-d9cd-891f-f32b-d027c4029d02>
2012-01-30 16:07:36 +0000

    geburtstage

--- trunk/libmediawiki/geburtstage.py
+++ trunk/libmediawiki/geburtstage.py
@@ -14,9 +14,8 @@ pw	= parser.get('test', 'password')
 
 wiki = MediaWiki(host, port, db, user, pw, debug=False)
 
-print [u.user_name for u in wiki.getUsers()]
-#print str(len(wiki.getPages()))+' pages total'
-print [c.page_title for c in wiki.getPages(namespace=NS_IMAGE)]
-print [p.page_title for p in wiki.getPages(wiki.getPage(title='Geburtstage'), namespace=NS_MAIN)]
+for page in wiki.getPages( parent=wiki.getPage(title='Geburtstage') ):
+	for line in str(page.getText()).split('\n'):
+		if line.find('{{Geburtstag') > -1:
+			print page.page_title+': '+line
 
-print [r.rev_comment for r in wiki.getPage(title='Geburtstage', namespace=NS_CATEGORY).getRevisions()]
